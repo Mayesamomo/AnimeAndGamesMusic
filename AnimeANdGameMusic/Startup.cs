3275c7b31ffe9d02b5f26f7dbfd711fb02d4acf0
Michael A. Momo <mayesamomo1997@gmail.com>
2023-08-07 13:47:22 -0400

    initialCommit

---
+++ AnimeANdGameMusic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnimeANdGameMusic.Startup))]
+namespace AnimeANdGameMusic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
