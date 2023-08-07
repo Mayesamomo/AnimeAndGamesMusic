ee489b24d59832c5d1e6bb26abc4f004f3166aab
Michael A. Momo <mayesamomo1997@gmail.com>
2023-08-07 14:05:29 -0400

    models added

---
+++ AnimeANdGameMusic/Models/Artist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AnimeANdGameMusic.Models
+{
+    public class Artist
+    {
+        [Key]
+        public int ArtistId { get; set; }
+
+        [Required(ErrorMessage = "The Artist Name is required.")]
+        [StringLength(100, ErrorMessage = "The Artist Name must be between 1 and 100 characters.", MinimumLength = 1)]
+        public string ArtistName { get; set; }
+
+        // one-to-many relationship between Artist and Album
+        public ICollection<Album> Albums { get; set; }
+
+        //dto class
+        public class ArtistDto
+        {
+            public int ArtistId { get; set; }
+            public string ArtistName { get; set; }
+        }
+
+
+    }
+}
---
+++ AnimeANdGameMusic/Models/Genre.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AnimeANdGameMusic.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreId { get; set; }
+
+        [Required(ErrorMessage = "The Genre Name is required.")]
+        [StringLength(50, ErrorMessage = "The Genre Name must be between 1 and 50 characters.", MinimumLength = 1)]
+        public string GenreTitle { get; set; }
+
+        //one-to-many relationship between Genre and Song
+        //genre has many songs 
+        public ICollection<Song> Songs { get; set; }
+    }
+
+    //dto class
+    public class GenreDto
+    {
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+    }
+}
