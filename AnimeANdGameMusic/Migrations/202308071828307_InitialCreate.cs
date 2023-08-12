f7bc0085671f4018c5b46b8305e32e3292fcb708
williamphk <willphk@outlook.com>
2023-08-12 02:29:50 -0400

    Fixed InitialCreate Migration

--- AnimeANdGameMusic/Migrations/202308071828307_InitialCreate.cs
+++ AnimeANdGameMusic/Migrations/202308071828307_InitialCreate.cs
@@ -2,71 +2,98 @@
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class InitialCreate : DbMigration
     {
         public override void Up()
         {
             CreateTable(
-                "dbo.Albums",
+                "dbo.AspNetRoles",
                 c => new
-                    {
-                        AlbumId = c.Int(nullable: false, identity: true),
-                        AlbumTitle = c.String(nullable: false, maxLength: 100),
-                        ReleaseYear = c.Int(nullable: false),
-                        ArtistId = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => t.AlbumId)
-                .ForeignKey("dbo.Artists", t => t.ArtistId, cascadeDelete: true)
-                .Index(t => t.ArtistId);
-            
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Name = c.String(nullable: false, maxLength: 256),
+                })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+
             CreateTable(
-                "dbo.Artists",
+                "dbo.AspNetUserRoles",
                 c => new
-                    {
-                        ArtistId = c.Int(nullable: false, identity: true),
-                        ArtistName = c.String(nullable: false, maxLength: 100),
-                    })
-                .PrimaryKey(t => t.ArtistId);
-            
+                {
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    RoleId = c.String(nullable: false, maxLength: 128),
+                })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+
             CreateTable(
-                "dbo.Songs",
+                "dbo.AspNetUsers",
                 c => new
-                    {
-                        SongId = c.Int(nullable: false, identity: true),
-                        SongTitle = c.String(nullable: false, maxLength: 100),
-                        AlbumId = c.Int(nullable: false),
-                        GenreId = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => t.SongId)
-                .ForeignKey("dbo.Albums", t => t.AlbumId, cascadeDelete: true)
-                .ForeignKey("dbo.Genres", t => t.GenreId, cascadeDelete: true)
-                .Index(t => t.AlbumId)
-                .Index(t => t.GenreId);
-            
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Email = c.String(maxLength: 256),
+                    EmailConfirmed = c.Boolean(nullable: false),
+                    PasswordHash = c.String(),
+                    SecurityStamp = c.String(),
+                    PhoneNumber = c.String(),
+                    PhoneNumberConfirmed = c.Boolean(nullable: false),
+                    TwoFactorEnabled = c.Boolean(nullable: false),
+                    LockoutEndDateUtc = c.DateTime(),
+                    LockoutEnabled = c.Boolean(nullable: false),
+                    AccessFailedCount = c.Int(nullable: false),
+                    UserName = c.String(nullable: false, maxLength: 256),
+                })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+
             CreateTable(
-                "dbo.Genres",
+                "dbo.AspNetUserClaims",
                 c => new
-                    {
-                        GenreId = c.Int(nullable: false, identity: true),
-                        GenreTitle = c.String(nullable: false, maxLength: 50),
-                    })
-                .PrimaryKey(t => t.GenreId);
-            
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    ClaimType = c.String(),
+                    ClaimValue = c.String(),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                {
+                    LoginProvider = c.String(nullable: false, maxLength: 128),
+                    ProviderKey = c.String(nullable: false, maxLength: 128),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+
         }
-        
+
         public override void Down()
         {
-            DropForeignKey("dbo.Songs", "GenreId", "dbo.Genres");
-            DropForeignKey("dbo.Songs", "AlbumId", "dbo.Albums");
-            DropForeignKey("dbo.Albums", "ArtistId", "dbo.Artists");
-            DropIndex("dbo.Songs", new[] { "GenreId" });
-            DropIndex("dbo.Songs", new[] { "AlbumId" });
-            DropIndex("dbo.Albums", new[] { "ArtistId" });
-            DropTable("dbo.Genres");
-            DropTable("dbo.Songs");
-            DropTable("dbo.Artists");
-            DropTable("dbo.Albums");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetRoles");
         }
     }
 }
--- AnimeANdGameMusic/Migrations/202308071831187_Updated.designer.cs
+++ AnimeANdGameMusic/Migrations/202308071831187_Updated.designer.cs
@@ -7,13 +7,13 @@ namespace AnimeANdGameMusic.Migrations
     using System.Resources;
 
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class InitialCreate : IMigrationMetadata
+    public sealed partial class Updated : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Updated));
 
         string IMigrationMetadata.Id
         {
-            get { return "202308071802034_InitialCreate"; }
+            get { return "202308071831187_Updated"; }
         }
 
         string IMigrationMetadata.Source
